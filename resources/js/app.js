8a2d3cd78ccbe5208f25e75c465a6938a4163867
Tritchaphorn BOISSON <tritchaphorn@gmail.com>
2024-01-29 18:57:08 +0100

    Implement vue on the frontend

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,4 +4,15 @@ import { createApp } from 'vue';
 
 import App from './vue/app.vue';
 
-createApp(App).mount("#app")
+
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faPlusCircle, faPlusSquare, faTrash } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faPlusSquare, faTrash, faPlusCircle)
+
+const app = createApp(App);
+app.component('font-awesome-icon', FontAwesomeIcon);
+app.mount("#app");
+
